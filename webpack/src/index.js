4f82b71064624345cc6faf997e7d813bfbbf384e
fdf_100000581038_fengchuanyu <fdf_100000581038_fengchuanyu@git.cloud.tencent.com>
2019-01-23 16:21:49 +0800

    admin

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -1,1 +1,4 @@
+import css from "./css/main.css"
 let a = 10;
+var oBox = document.querySelector("#box");
+oBox.innerHTML = "12346"
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,4 +1,8 @@
 const path = require('path');
+const webpack = require("webpack");
+const htmlPlugin = require("html-webpack-plugin");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
 module.exports={
   mode:"development",
   entry:{
@@ -6,15 +10,50 @@ module.exports={
   },
   output:{
     path:path.resolve(__dirname,'dist'),
-    filename:"[name].js"
+    filename:"[name].js",
+    publicPath:"http://127.0.0.1:8081/"
   },
   module:{
-
+    rules:[
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: "css-loader"
+        })
+      },
+      {
+        test:/\.(png|jpg|gif)$/,
+        use:[{
+          loader:"url-loader",
+          options:{
+            limit:500,
+            outputPath:'./img/'
+          }
+        }]
+      },{
+        test:/\.(html|htm)/i,
+        loader:'html-withimg-loader'
+      }
+    ]
   },
   plugins:[
-    
+    new webpack.HotModuleReplacementPlugin(),
+    new htmlPlugin({
+      minify:{
+        removeAttributeQuotes:true,
+        collapseWhitespace:false
+      },
+      hash:true,
+      template:"./src/index.html"
+    }),
+    new ExtractTextPlugin("./css/main.css"),
   ],
   devServer:{
-
+    contentBase: path.resolve(__dirname,'dist'),
+    host:"127.0.0.1",
+    compress:true,
+    port:"8081",
+    hot:true
   }
 }
