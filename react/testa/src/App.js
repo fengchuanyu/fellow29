aca5c30e660fa4355afb1ce4e9e1bcb8c75f94f5
fdf_100000581038_fengchuanyu <fdf_100000581038_fengchuanyu@git.cloud.tencent.com>
2019-02-22 14:51:21 +0800

    admin

--- react/testa/src/App.js
+++ react/testa/src/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
+const Test = ()=><h1>hello world</h1>
 class App extends Component {
   render() {
+
     return (
       <div className="App">
         <header className="App-header">
