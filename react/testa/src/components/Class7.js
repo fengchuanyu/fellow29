d9cbef90e411587d40e83b94b3af70f1eeaa3883
fdf_100000581038_fengchuanyu <fdf_100000581038_fengchuanyu@git.cloud.tencent.com>
2019-02-27 11:42:30 +0800

    admin

--- react/testa/src/components/Class7.js
+++ react/testa/src/components/Class7.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import axios from 'axios'
+import URL from '../service.config'
 
 export default class Class7 extends Component {
   constructor(props) {
@@ -12,7 +13,7 @@ export default class Class7 extends Component {
     let url = 'https://api.myjson.com/bins/9inum';
     let url1 = 'https://www.fellow29.com/getData';
     let url2 = 'https://www.fellow29.com/getData2';
-    let url3 = 'https://www.fellow29.com/getData3'
+    let url3 = URL.getData3
     axios.get(url3).then((res)=>{
       // com = res.data.data.map((item,index)=>{
       //   return(
--- react/testa/src/service.config.js
+++ react/testa/src/service.config.js
@@ -1,16 +1,12 @@
 const BASE_URL={
-  dev:"http://www.fellow29.com/",
-  pro:"http://www.xxx.com/"
+  dev:'https://www.fellow29.com/',
+  pro:'https://www.xxx.com/'
 }
 
-let globalUrl = BASE_URL.dev
+let globalUrl = BASE_URL.dev;
 
 const URL = {
-  getData1:globalUrl+"getData",
-  getData2:globalUrl+"getData2",
-  getData3:globalUrl+"getData3",
-  getData4:globalUrl+"getData4",
-  getData5:globalUrl+"getData5",
+  getData3:globalUrl+"getData3"
 }
 
 export default URL;
